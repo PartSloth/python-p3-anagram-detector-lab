c18aaae8802e01f410b559b2f7561547270d4b6a
Louiza Mak <louiza.mak@gmail.com>
2024-04-23 15:01:03 -0500

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, l):
+        split_word = list(self.word)
+        split_word.sort()
+        match_list = []
+        for word in l:
+            split_match = list(word)
+            split_match.sort()
+            if(split_match == split_word):
+                match_list.append(word)
+        return match_list
